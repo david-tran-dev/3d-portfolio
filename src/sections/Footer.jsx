29d62b2df49605052e47431af26d88637d786c0f
David TRAN <david.tran.devweb@gmail.com>
2024-09-15 22:22:47 +0200

    add social links

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -8,15 +8,35 @@ const Footer = () => {
       </div>
 
       <div className="flex gap-3">
-        <div className="social-icon">
+        <a
+          href="https://github.com/david-tran-dev"
+          target="_blank"
+          rel="noreferrer"
+          className="social-icon">
           <img src="/assets/github.svg" alt="github" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
-          <img src="/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2" />
-        </div>
-        <div className="social-icon">
-          <img src="/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2" />
-        </div>
+        </a>
+        <a
+          href="https://x.com/DavidT_Dev"
+          target="_blank"
+          rel="noreferrer"
+          className="social-icon">
+          <img
+            src="/assets/twitter.svg"
+            alt="twitter"
+            className="w-1/2 h-1/2"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/david-tran-dev"
+          target="_blank"
+          rel="noreferrer"
+          className="social-icon">
+          <img
+            src="/assets/linkedIn-logo.png"
+            alt="instagram"
+            className="w-1/2 h-1/2"
+          />
+        </a>
       </div>
 
       <p className="text-white-500">© 2024 David TRAN. All rights reserved.</p>
